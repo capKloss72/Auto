38e6733ef0ac99b77418cf7c8eae0156f2269701
Tym Zon <tym.zon@gmail.com>
2020-08-21 11:53:12 +0800

    removed tags

--- src/test/java/com/capkloss/cucumber/options/TestRunner.java
+++ src/test/java/com/capkloss/cucumber/options/TestRunner.java
@@ -8,9 +8,8 @@ import io.cucumber.junit.CucumberOptions;
 @RunWith(Cucumber.class)
 @CucumberOptions(
         features = "src/test/java/features",
-        plugin = {"pretty", "html:target/cucumber.html", "json:target/cucumber.json"},
-        glue = "com.capkloss.cucumber.stepdefinitions",
-        tags = "@RegTest")
+        plugin = {"pretty", "html:target/cucumber.html", "json:target/cucumber.json", "junit:target/cukes.xml"},
+        glue = "com.capkloss.cucumber.stepdefinitions")
 public class TestRunner {
 
 }
